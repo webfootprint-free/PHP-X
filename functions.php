e006e6854ad6cac30c7bc7757c7b4326afeec391
Mark Grenville <mark@webfootprint.co.za>
2019-02-08 02:06:25 +0200

    Added functions

--- functions.php
+++ functions.php
@@ -6,6 +6,73 @@
         error_reporting(E_ALL);
     }
 
+    function x_set(&$var) {
+        if (!empty($var)) {
+            return $var;
+        }
+        return '';
+    }
+
+    function x_truncate($string, $length, $dots = "...") {
+	    return (strlen($string) > $length) ? substr($string, 0, $length - strlen($dots)) . $dots : $string;
+	}
+
+    function x_time_elapsed($datetime, $timestamp = true)
+    {
+        if (empty($timestamp)) {
+            $ptime = $datetime;
+        } else {
+            $ptime = strtotime($datetime);
+        }
+        $etime = time() - $ptime;
+
+        if ($etime < 1)
+        {
+            $timeago = '0 seconds';
+        } else {
+            $a = array( 12 * 30 * 24 * 60 * 60  =>  'year',
+                        30 * 24 * 60 * 60       =>  'month',
+                        24 * 60 * 60            =>  'day',
+                        60 * 60                 =>  'hour',
+                        60                      =>  'minute',
+                        1                       =>  'second'
+                        );
+
+            foreach ($a as $secs => $str)
+            {
+                $d = $etime / $secs;
+                if ($d >= 1)
+                {
+                    $r = round($d);
+                    $timeago = $r . ' ' . $str . ($r > 1 ? 's' : '') . ' ago';
+                    break;
+                }
+            }
+        }
+
+        $human_readable = human_date ($datetime);
+        $output = '<a title="'.$human_readable.'">'.$timeago.'</a>';
+        return $output;
+    }
+
+    function x_curl($url){
+	    if (!function_exists('curl_init')){
+	        die('Sorry cURL is not installed!');
+	    }
+	    $ch = curl_init();
+	    curl_setopt($ch, CURLOPT_URL, $url);
+	    curl_setopt($ch, CURLOPT_HEADER, 0);
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	    curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_FAILONERROR, false);
+		curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+	    $output = curl_exec($ch);
+	    curl_close($ch);
+	    return $output;
+	}
+
     function x_curl_cache($actual_url, $expires = null){
         global $x_override_curl_cache;
 
